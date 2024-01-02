a5712daa52ed489b419208c86d1f7c2fe17db1f5
franck403 <79372051+franck403@users.noreply.github.com>
2024-01-01 19:41:42 -0500

    Update sw.vfs.js

--- sw.vfs.js
+++ sw.vfs.js
@@ -1,15 +1,3 @@
-// Register the Service Worker
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(function (registration) {
-      console.log('Service Worker registered with scope:', registration.scope);
-    })
-    .catch(function (error) {
-      console.error('Service Worker registration failed:', error);
-    });
-}
-
-// sw.js file
 self.addEventListener('fetch', function (event) {
   var url = new URL(event.request.url);
 
