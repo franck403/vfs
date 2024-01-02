87ac55059fb01afe1b708ee3ee32e3ce4f251bbc
franck403 <79372051+franck403@users.noreply.github.com>
2024-01-01 19:36:53 -0500

    Create vfs.js

---
+++ vfs.js
@@ -0,0 +1,43 @@
+function VFS() {
+  this.files = {};
+  this.proxyPath = '/vfs'; // Proxy path for file requests
+}
+VFS.prototype.addFile = function (filename, content) {
+  this.files[filename] = content;
+};
+
+VFS.prototype.deleteFile = function (filename) {
+  delete this.files[filename];
+};
+
+VFS.prototype.readFile = function (filename) {
+  return this.files[filename];
+};
+// Register the Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(function (registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(function (error) {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
+// sw.js file
+self.addEventListener('fetch', function (event) {
+  var url = new URL(event.request.url);
+
+  // Intercept requests to the proxy path
+  if (url.pathname.startsWith('/vfs')) {
+    var filename = url.pathname.substring(4); // Remove '/vfs' from the path
+    var vfs = new VFS();
+
+    // Check if the file exists in the VFS
+    if (vfs.files.hasOwnProperty(filename)) {
+      var content = vfs.readFile(filename);
+      var response = new Response(content);
+      event.respondWith(response);
+    }
+  }
+});
